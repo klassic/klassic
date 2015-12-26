ecf022446fc9a5b003aa38f870d1593e50d0e72f
Kota Mizushima <mizukota@gmail.com>
2015-12-26 17:55:57 +0900

    TypeConstructor and LambdaTypeConstructor (e. g. [X] => List[X]).

--- src/main/scala/com/github/kmizu/klassic/ast/package.scala
+++ src/main/scala/com/github/kmizu/klassic/ast/package.scala
@@ -7,8 +7,11 @@ package object ast {
   case class Location(line: Int, column: Int)
 
   sealed abstract class Type extends AstNode
+  case class SimpleType(location: Location, name: String) extends AstNode
+  case class TypeConstructor(location: Location, constructor: Type, args: List[Type]) extends AstNode
+  case class LambdaTypeConstructor(location: Location, args: List[Type], body: Type) extends AstNode
 
   sealed abstract class Expression extends AstNode
-  case class VariadicLiteral[T](value: T) extends Expression
+  case class VariadicLiteral[T](location: Location, value: T) extends Expression
   case class TypedExpression(location: Location, body: Expression, annotation: Type) extends Expression
 }
