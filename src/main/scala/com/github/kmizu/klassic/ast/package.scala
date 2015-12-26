b10b8d5c2ac63542ab740a6e9ebb674628aea93c
Kota Mizushima <mizukota@gmail.com>
2015-12-26 16:47:58 +0900

    Expression and TypedExpression (Note that their definitions maybe changed)

--- src/main/scala/com/github/kmizu/klassic/ast/package.scala
+++ src/main/scala/com/github/kmizu/klassic/ast/package.scala
@@ -5,4 +5,9 @@ package object ast {
     def location: Location
   }
   case class Location(line: Int, column: Int)
+
+  sealed abstract class Type extends AstNode
+
+  sealed abstract class Expression extends AstNode
+  case class TypedExpression(location: Location, body: Expression, annotation: Type) extends Expression
 }
