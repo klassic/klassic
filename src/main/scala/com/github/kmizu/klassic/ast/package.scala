d1f1bd85306f51112aa2e3c9474e67d0dd16d5c1
Kota Mizushima <mizukota@gmail.com>
2015-12-30 00:32:26 +0900

    BlockExpression, which represents sequential evaluations.

--- src/main/scala/com/github/kmizu/klassic/ast/package.scala
+++ src/main/scala/com/github/kmizu/klassic/ast/package.scala
@@ -13,6 +13,7 @@ package object ast {
 
   sealed abstract class Expression extends AstNode
   case class IfExpression(location: Location, condition: Expression, thenClause: Expression, elseClause: Expression) extends Expression
+  case class BlockExpression(location: Location, expressions: List[Expression]) extends Expression
   case class MethodCall(location: Location, receiver: Expression, name: String, params: List[Expression]) extends Expression
   case class VariadicLiteral[T](location: Location, value: T) extends Expression
   case class TypedExpression(location: Location, body: Expression, annotation: Type) extends Expression
