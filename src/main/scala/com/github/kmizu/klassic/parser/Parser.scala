e952864bfe82a6ad0ce4e99ff7e188849fc83068
Kota Mizushima <mizukota@gmail.com>
2016-04-18 07:48:18 +0900

    Start to define tokens.

--- src/main/scala/com/github/kmizu/klassic/parser/Parser.scala
+++ src/main/scala/com/github/kmizu/klassic/parser/Parser.scala
@@ -6,22 +6,34 @@ import fastparse.all._
 object Parser {
 
 
-  val hexDigit      = P( CharIn('0'to'9', 'a'to'f', 'A'to'F') )
-  val unicodeEscape = P( "u" ~ hexDigit ~ hexDigit ~ hexDigit ~ hexDigit )
-  val escape        = P( "\\" ~ (CharIn("\"/\\bfnrt") | unicodeEscape) )
+  val hexDigit      = P(CharIn('0' to '9', 'a' to 'f', 'A' to 'F'))
+  val unicodeEscape = P("u" ~ hexDigit ~ hexDigit ~ hexDigit ~ hexDigit)
+  val escape        = P("\\" ~ (CharIn("\"/\\bfnrt") | unicodeEscape))
   val chars         = P(CharsWhile(!"\"\\".contains(_: Char)))
-  val spacing       = P( CharsWhile(!"\r\n\t\f\b".contains(_:Char)).? )
+  val spacing       = P(CharsWhile(!"\r\n\t\f\b".contains(_: Char)).?)
 
-  val expression: P[Ast.Expression] = P(literal)
-  val literal: P[Ast.Expression] = P((integer | double | string | bool | identifier) ~ spacing)
-  val integer: P[Ast.IntValue] = P(("-"|"+").? ~ CharIn('0'to'9').rep(1)).!.map{v => Ast.IntValue(v.toInt)}
-  val double: P[Ast.DoubleValue] = P(("-"|"+").? ~ (CharIn('0'to'9').rep(1) ~ "." ~ CharIn('0'to'9').rep(1)).!.map{v => Ast.DoubleValue(v.toDouble)})
-  val string: P[Ast.StringValue] = P("\"" ~ (chars | escape).rep(0).! ~ "\"").map(Ast.StringValue(_))
-  val bool: P[Ast.BoolValue] = P(("true".!.map{_ => true} | "false".!.map{_ => false}).map{Ast.BoolValue(_)})
-  val identifier: P[Ast.Identifier] = P{
-    (identifierFirst ~ (identifierFirst | P(CharIn('0'to'9'))).rep(0)).!.map{Ast.Identifier(_)}
+  def token(value: String) = value ~ spacing
+
+  val klazz   = token("class")
+  val traiz   = token("trait")
+  val subtype = token("<:")
+  val lbrace  = token("{")
+  val rbrace  = token("}")
+
+  val expression     : P[Ast.Expression]  = P(literal)
+  val literal        : P[Ast.Expression]  = P((integer | double | string | bool | identifier) ~ spacing)
+  val integer        : P[Ast.IntValue]    = P(("-" | "+").? ~ CharIn('0' to '9').rep(1)).!.map { v => Ast.IntValue(v.toInt) }
+  val double         : P[Ast.DoubleValue] = P(("-" | "+").? ~ (CharIn('0' to '9').rep(1) ~ "." ~ CharIn('0' to '9').rep(1)).!.map { v => Ast.DoubleValue(v.toDouble) })
+  val string         : P[Ast.StringValue] = P("\"" ~ (chars | escape).rep(0).! ~ "\"").map(Ast.StringValue(_))
+  val bool           : P[Ast.BoolValue]   = P(("true".!.map { _ => true } | "false".!.map { _ => false }).map {
+    Ast.BoolValue(_)
+  })
+  val identifier     : P[Ast.Identifier]  = P {
+    (identifierFirst ~ (identifierFirst | P(CharIn('0' to '9'))).rep(0)).!.map {
+      Ast.Identifier(_)
+    }
   }
-  val identifierFirst: P[String] = P {
-    CharIn('a' to 'z', 'A' to 'Z', List('_', '-', '+', '*', '/', '#', '$', '&', '!', '?', '%', '|', '=', '^', ':')).!
+  val identifierFirst: P[String]          = P {
+    CharIn('a' to 'z', 'A' to 'Z', List('_')).!
   }
 }
