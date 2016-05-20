04e516a8ef8400f6cb787a3d47fb855ec215aeeb
Kota Mizushima <mizukota@gmail.com>
2016-05-20 23:30:12 +0900

    Operator enum classes.

---
+++ src/main/scala/org/onion_lang/toys/Operator.scala
@@ -0,0 +1,16 @@
+package org.onion_lang.toys
+
+/**
+  * Created by Mizushima on 2016/05/20.
+  */
+sealed abstract class Operator(val descriptor: String)
+object Operator {
+  case object Add extends Operator("+")
+  case object Subtract extends Operator("-")
+  case object Divide extends Operator("/")
+  case object Multiply extends Operator("*")
+  case object LessThan extends Operator("<")
+  case object LessOrEqual extends Operator("<=")
+  case object GreaterThan extends Operator(">")
+  case object GreaterOrEqual extends Operator(">=")
+}
