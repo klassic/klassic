c83e60e117e0d869a107b6ea42abe5a099ada1a5
Kota Mizushima <mizukota@gmail.com>
2016-05-30 20:40:38 +0900

    Implement line comments and block(nestable) comments

---
+++ src/test/scala/com/github/klassic/CommentSpec.scala
@@ -0,0 +1,51 @@
+package com.github.klassic
+
+class CommentSpec extends SpecHelper {
+  val I = new Interpreter
+
+  describe("line comment") {
+    val expectations = List[(String, Value)](
+      """|//line comment
+         |1
+      """.stripMargin -> BoxedInt(1),
+      """|1
+         |//line comment
+      """.stripMargin -> BoxedInt(1),
+      """|1 +
+         |//line comment
+         |2
+      """.stripMargin -> BoxedInt(3),
+      """|1
+         |//line comment
+         |+ 2""".stripMargin -> BoxedInt(2)
+    )
+    expectations.zipWithIndex.foreach{ case ((in, expected), i) =>
+      it(s"expectations ${i}") {
+        assert(expected == I.evaluateString(in))
+      }
+    }
+
+    describe("block comment") {
+      val expectations = List[(String, Value)](
+        """|1 + /* block comment */
+           |1
+        """.stripMargin -> BoxedInt(2),
+        """|1 /* + block comment */
+           |1
+        """.stripMargin -> BoxedInt(1),
+        """|/*/**/*/
+           |1
+        """.stripMargin -> BoxedInt(1),
+        """|1 /* nested
+           |     /* comment */
+           |   here */ + 2
+        """.stripMargin -> BoxedInt(3)
+      )
+      expectations.zipWithIndex.foreach{ case ((in, expected), i) =>
+        it(s"expectations ${i}") {
+          assert(expected == I.evaluateString(in))
+        }
+      }
+    }
+  }
+}
