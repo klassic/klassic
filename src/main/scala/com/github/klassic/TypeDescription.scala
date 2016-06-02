ea18d4a3a7dc572e52389fbc7e2810c20100f27e
Kota Mizushima <mizukota@gmail.com>
2016-06-02 16:03:58 +0900

    Type checking is continued

--- src/main/scala/com/github/klassic/TypeDescription.scala
+++ src/main/scala/com/github/klassic/TypeDescription.scala
@@ -9,6 +9,10 @@ object TypeDescription {
   case object FloatType extends TypeDescription("Float")
   case object DoubleType extends TypeDescription("Double")
   case object BooleanType extends TypeDescription("Boolean")
+  case object UnitType extends TypeDescription("Unit")
   case object DynamicType extends TypeDescription("*")
   case object UnknownType extends TypeDescription("?")
+  case object ErrorType extends TypeDescription("!")
+  case class FunctionType(paramTypes: List[TypeDescription], returnType: TypeDescription) extends
+    TypeDescription(s"(${paramTypes.mkString(", ")}) => ${returnType}")
 }
