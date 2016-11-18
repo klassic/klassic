02ab5646d4b44e5e5873b1858e2d512731d4906d
Kota Mizushima <mizukota@gmail.com>
2016-11-18 17:26:06 +0900

    A FormalParameter has optionalType insteda of description

--- src/main/scala/com/github/klassic/FormalParameter.scala
+++ src/main/scala/com/github/klassic/FormalParameter.scala
@@ -1,4 +1,4 @@
 package com.github.klassic
 
 import com.github.klassic.TypeDescription.DynamicType
-case class FormalParameter(name: String, description: TypeDescription = DynamicType)
+case class FormalParameter(name: String, optionalType: Option[TypeDescription])
