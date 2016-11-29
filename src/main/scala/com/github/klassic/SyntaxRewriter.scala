80ff747377e035bbb9f0d2249b2a9bde32ddd82f
Kota Mizushima <mizukota@gmail.com>
2016-11-29 16:56:26 +0900

    Introduce AST of VariantDeclaration

--- src/main/scala/com/github/klassic/SyntaxRewriter.scala
+++ src/main/scala/com/github/klassic/SyntaxRewriter.scala
@@ -22,6 +22,8 @@ class SyntaxRewriter {
           List(Let(location, variable, description, doRewrite(value), Block(location, rewriteBlock(xs)), immutable))
         case FunctionDefinition(loation, name, expression, cleanup) :: xs =>
           List(LetRec(location, name, doRewrite(expression).asInstanceOf[AST.Lambda], cleanup.map(doRewrite), Block(location, rewriteBlock(xs))))
+        case (x@VariantDeclaration(_, _, _, _)) :: xs =>
+          List(VariantIn(x.location, x, Block(location, rewriteBlock(xs))))
         case x :: xs =>
           doRewrite(x) :: rewriteBlock(xs)
         case Nil =>
