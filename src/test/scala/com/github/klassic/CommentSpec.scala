6a5bfbcc4e0d631d62af6172a034d06a82957894
Kota Mizushima <mizukota@gmail.com>
2017-07-23 00:57:11 +0900

    Refactor tests
    CommentSpec and ExpressionSpec are refactored.

--- src/test/scala/com/github/klassic/CommentSpec.scala
+++ src/test/scala/com/github/klassic/CommentSpec.scala
@@ -1,49 +1,55 @@
 package com.github.klassic
 
 class CommentSpec extends SpecHelper {
-  describe("line comment") {
-    val expectations = List[(String, Value)](
-      """|//line comment
-         |1
-      """.stripMargin -> BoxedInt(1),
-      """|1
-         |//line comment
-      """.stripMargin -> BoxedInt(1),
-      """|1 +
-         |//line comment
-         |2
-      """.stripMargin -> BoxedInt(3),
-      """|1
-         |//line comment
-         |+ 2""".stripMargin -> BoxedInt(2)
-    )
-    expectations.zipWithIndex.foreach{ case ((in, expected), i) =>
-      it(s"expectations ${i}") {
-        assert(expected == E(in))
-      }
+  describe("line comments") {
+    it("should be parsed correctly") {
+      assertResult(
+        E("""|//line comment
+           |1
+          """.stripMargin)
+      )(BoxedInt(1))
+      assertResult(
+        E("""|1
+            |//line comment
+          """.stripMargin)
+      )(BoxedInt(1))
+      assertResult(
+        E("""|1 +
+            |//line comment
+            |2
+        """.stripMargin)
+      )(BoxedInt(3))
+      assertResult(
+        E("""|1
+            |//line comment
+            |+ 2""".stripMargin)
+      )(BoxedInt(2))
     }
+  }
 
-    describe("block comment") {
-      val expectations = List[(String, Value)](
-        """|1 + /* block comment */
+  describe("block comments") {
+    it("should be parsed correctly") {
+      assertResult(
+        E("""|1 + /* block comment */
            |1
-        """.stripMargin -> BoxedInt(2),
-        """|1 /* + block comment */
+        """.stripMargin)
+      )(BoxedInt(2))
+      assertResult(
+        E("""|1 /* + block comment */
            |1
-        """.stripMargin -> BoxedInt(1),
-        """|/*/**/*/
-           |1
-        """.stripMargin -> BoxedInt(1),
-        """|1 /* nested
+        """.stripMargin)
+      )(BoxedInt(1))
+      assertResult(
+        E("""|/*/**/*/
+            |1
+        """.stripMargin)
+      )(BoxedInt(1))
+      assertResult(
+        E("""|1 /* nested
            |     /* comment */
            |   here */ + 2
-        """.stripMargin -> BoxedInt(3)
-      )
-      expectations.zipWithIndex.foreach{ case ((in, expected), i) =>
-        it(s"expectations ${i}") {
-          assert(expected == E(in))
-        }
-      }
+        """.stripMargin)
+      )(BoxedInt(3))
     }
   }
 }
