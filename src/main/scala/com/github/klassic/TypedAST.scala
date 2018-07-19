ea87c5e5c65b2fcb77766b5d09e69b1376e183f2
Kota Mizushima <mizukota@gmail.com>
2018-07-19 17:04:22 +0900

    Remove redundant val declaration

--- src/main/scala/com/github/klassic/TypedAST.scala
+++ src/main/scala/com/github/klassic/TypedAST.scala
@@ -26,71 +26,71 @@ object TypedAST {
 
   case object FloatSuffix extends FloatSuffix
 
-  case class Program(val location: Location, imports: List[Import], block: Block, records: RecordEnvironment)
+  case class Program(location: Location, imports: List[Import], block: Block, records: RecordEnvironment)
 
-  case class Import(val location: Location, simpleName: String, fqcn: String)
+  case class Import(location: Location, simpleName: String, fqcn: String)
 
-  case class Block(val type_ : Type, val location: Location, expressions: List[TypedAST]) extends TypedAST
+  case class Block(type_ : Type, location: Location, expressions: List[TypedAST]) extends TypedAST
 
-  case class IfExpression(val type_ : Type, val location: Location, condition: TypedAST, thenExpression: TypedAST, elseExpression: TypedAST) extends TypedAST
+  case class IfExpression(type_ : Type, location: Location, condition: TypedAST, thenExpression: TypedAST, elseExpression: TypedAST) extends TypedAST
 
-  case class ForeachExpression(val type_ : Type, val location: Location, name: String, collection: TypedAST, body: TypedAST) extends TypedAST
+  case class ForeachExpression(type_ : Type, location: Location, name: String, collection: TypedAST, body: TypedAST) extends TypedAST
 
-  case class BinaryExpression(val type_ : Type, val location: Location, operator: Operator, lhs: TypedAST, rhs: TypedAST) extends TypedAST
+  case class BinaryExpression(type_ : Type, location: Location, operator: Operator, lhs: TypedAST, rhs: TypedAST) extends TypedAST
 
-  case class WhileExpression(val type_ : Type, val location: Location, condition: TypedAST, body: TypedAST) extends TypedAST
+  case class WhileExpression(type_ : Type, location: Location, condition: TypedAST, body: TypedAST) extends TypedAST
 
-  case class MinusOp(val type_ : Type, val location: Location, operand: TypedAST) extends TypedAST
+  case class MinusOp(type_ : Type, location: Location, operand: TypedAST) extends TypedAST
 
-  case class PlusOp(val type_ : Type, val location: Location, operand: TypedAST) extends TypedAST
+  case class PlusOp(type_ : Type, location: Location, operand: TypedAST) extends TypedAST
 
-  case class StringNode(val type_ : Type, val location: Location, value: String) extends TypedAST
+  case class StringNode(type_ : Type, location: Location, value: String) extends TypedAST
 
-  case class IntNode(val type_ : Type, val location: Location, value: Int) extends TypedAST
+  case class IntNode(type_ : Type, location: Location, value: Int) extends TypedAST
 
-  case class LongNode(val type_ : Type, val location: Location, value: Long) extends TypedAST
+  case class LongNode(type_ : Type, location: Location, value: Long) extends TypedAST
 
-  case class ShortNode(val type_ : Type, val location: Location, value: Short) extends TypedAST
+  case class ShortNode(type_ : Type, location: Location, value: Short) extends TypedAST
 
-  case class ByteNode(val type_ : Type, val location: Location, value: Byte) extends TypedAST
+  case class ByteNode(type_ : Type, location: Location, value: Byte) extends TypedAST
 
-  case class BooleanNode(val type_ : Type, val location: Location, value: Boolean) extends TypedAST
+  case class BooleanNode(type_ : Type, location: Location, value: Boolean) extends TypedAST
 
-  case class DoubleNode(val type_ : Type, val location: Location, value: Double) extends TypedAST
+  case class DoubleNode(type_ : Type, location: Location, value: Double) extends TypedAST
 
-  case class FloatNode(val type_ : Type, val location: Location, value: Float) extends TypedAST
+  case class FloatNode(type_ : Type, location: Location, value: Float) extends TypedAST
 
-  case class Id(val type_ : Type, val location: Location, name: String) extends TypedAST
+  case class Id(type_ : Type, location: Location, name: String) extends TypedAST
 
-  case class Selector(val type_ : Type, val location: Location, module: String, name: String) extends TypedAST
+  case class Selector(type_ : Type, location: Location, module: String, name: String) extends TypedAST
 
-  case class Assignment(val type_ : Type, val location: Location, variable: String, value: TypedAST) extends TypedAST
+  case class Assignment(type_ : Type, location: Location, variable: String, value: TypedAST) extends TypedAST
 
-  case class LetDeclaration(val type_ : Type, val location: Location, variable: String, declaredType: Type, value: TypedAST, body: TypedAST, immutable: Boolean) extends TypedAST
+  case class LetDeclaration(type_ : Type, location: Location, variable: String, declaredType: Type, value: TypedAST, body: TypedAST, immutable: Boolean) extends TypedAST
 
-  case class FunctionLiteral(val type_ : Type, val location: Location, params: List[FormalParameterOptional], optionalType: Option[Type], proc: TypedAST) extends TypedAST {
+  case class FunctionLiteral(type_ : Type, location: Location, params: List[FormalParameterOptional], optionalType: Option[Type], proc: TypedAST) extends TypedAST {
     override def toString: String = s"""${params.mkString(", ")} => ${optionalType.getOrElse("?")}"""
   }
 
-  case class LetFunctionDefinition(val type_ : Type, val location: Location, name: String, body: FunctionLiteral, cleanup: Option[TypedAST], expression: TypedAST) extends TypedAST
+  case class LetFunctionDefinition(type_ : Type, location: Location, name: String, body: FunctionLiteral, cleanup: Option[TypedAST], expression: TypedAST) extends TypedAST
 
-  case class FunctionCall(val type_ : Type, val location: Location, func: TypedAST, params: List[TypedAST]) extends TypedAST
+  case class FunctionCall(type_ : Type, location: Location, func: TypedAST, params: List[TypedAST]) extends TypedAST
 
-  case class ListLiteral(val type_ : Type, val location: Location, elements: List[TypedAST]) extends TypedAST
+  case class ListLiteral(type_ : Type, location: Location, elements: List[TypedAST]) extends TypedAST
 
-  case class SetLiteral(val type_ : Type, val location: Location, elements: List[TypedAST]) extends TypedAST
+  case class SetLiteral(type_ : Type, location: Location, elements: List[TypedAST]) extends TypedAST
 
-  case class MapLiteral(val type_ : Type, val location: Location, elements: List[(TypedAST, TypedAST)]) extends TypedAST
+  case class MapLiteral(type_ : Type, location: Location, elements: List[(TypedAST, TypedAST)]) extends TypedAST
 
-  case class RecordSelect(val type_ : Type, val location: Location, expression: TypedAST, member: String) extends TypedAST
+  case class RecordSelect(type_ : Type, location: Location, expression: TypedAST, member: String) extends TypedAST
 
-  case class ObjectNew(val type_ : Type, val location: Location, className: String, params: List[TypedAST]) extends TypedAST
+  case class ObjectNew(type_ : Type, location: Location, className: String, params: List[TypedAST]) extends TypedAST
 
-  case class RecordNew(val type_ : Type, val location: Location, recordName: String, params: List[TypedAST]) extends TypedAST
+  case class RecordNew(type_ : Type, location: Location, recordName: String, params: List[TypedAST]) extends TypedAST
 
-  case class MethodCall(val type_ : Type, val location: Location, self: TypedAST, name: String, params: List[TypedAST]) extends TypedAST
+  case class MethodCall(type_ : Type, location: Location, self: TypedAST, name: String, params: List[TypedAST]) extends TypedAST
 
-  case class Casting(val type_ : Type, val location: Location, target: TypedAST, to: Type) extends TypedAST
+  case class Casting(type_ : Type, location: Location, target: TypedAST, to: Type) extends TypedAST
 
   case class ValueNode(value: Value) extends TypedAST {
     override val type_   : Type     = TDynamic
